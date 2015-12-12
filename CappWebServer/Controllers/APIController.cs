4a6698fbe763cd90df3894865c35e145ab6eefa4
Tulio de Souza <ss.tulio@gmail.com>
2015-12-12 19:37:52 -0200

    Criação da API

---
+++ CappWebServer/Controllers/APIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CappWebServer.Service;
+
+namespace CappWebServer.Controllers
+{
+    public class APIController : Controller
+    {
+        CAppService service;
+
+        public APIController()
+        {
+            service = new CAppService();
+        }
+
+        // GET: API
+        public ActionResult GetProva(string codigo)
+        {
+            Prova p = service.GetProva(codigo);
+            return Json(p, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CappWebServer/Service/CAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CappWebServer.Service
+{
+    public class CAppService
+    {
+
+        public Prova GetProva(string codigo)
+        {
+            using (CAppDataModel dc = new CAppDataModel())
+            {
+                Prova prova = dc.Prova.Where(p => p.CodigoProva.Equals(codigo)).FirstOrDefault();
+                return prova;
+            }
+        }
+
+    }
+}
